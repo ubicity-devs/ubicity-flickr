9903acbdf51ee180191667168bc062b924dc338a
Christoph Ruggenthaler <christoph.ruggenthaler@gmail.com>
2014-06-18 09:19:23 +0200

    ignore test

--- src/test/java/at/ac/ait/ubicity/ubicity/flickrplugin/FlickrTest.java
+++ src/test/java/at/ac/ait/ubicity/ubicity/flickrplugin/FlickrTest.java
@@ -2,12 +2,14 @@ package at.ac.ait.ubicity.ubicity.flickrplugin;
 
 import java.util.Date;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import at.ac.ait.ubicity.ubicity.flickrplugin.dto.FlickrDTO;
 
 public class FlickrTest {
 
+	@Ignore
 	@Test
 	public void JsonTest() {
 
